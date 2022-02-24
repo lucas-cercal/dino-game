17985c101d1cf2b01ca459b7b324bd53e3c5df51
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-02-24 14:07:22 -0300

    Adiciona a função pular ao elemento dino

--- script.js
+++ script.js
@@ -3,8 +3,37 @@ const dino = document.querySelector('.dino');
 function handleKeyUp(event)
 {
     if(event.keyCode === 32){
-        console.log("Pressionei espaço");
+        jump();
     }
+}
+
+function jump()
+{
+    let position = 0;
+
+    let upInterval = setInterval(() => {
+        if(position >= 150){
+
+            //Irá descer
+            clearInterval(upInterval);
+
+            let downInterval = setInterval(() => {
+
+                position -= 20;
+                dino.style.bottom = position + "px";
+
+                if(position == 0){
+                    clearInterval(downInterval);
+                }
+
+            }, 20)
+        } else{
+
+            //irá subir
+            position += 20;
+            dino.style.bottom = position + "px";
+        }
+    }, 20)
 }
 
 document.addEventListener('keyup', handleKeyUp);
