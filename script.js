a8bd080bfdec95b426ae0b0f9bf48dbfec411c96
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-02-24 14:23:06 -0300

    Adiciona validação para a função jump

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 const dino = document.querySelector('.dino');
+let isJumping = false;
 
 function handleKeyUp(event)
 {
     if(event.keyCode === 32){
-        jump();
+        if(!isJumping){
+            jump();
+        }
     }
 }
 
 function jump()
 {
+    isJumping = true;
     let position = 0;
-
     let upInterval = setInterval(() => {
         if(position >= 150){
-
             //Irá descer
             clearInterval(upInterval);
-
             let downInterval = setInterval(() => {
-
                 position -= 20;
                 dino.style.bottom = position + "px";
-
                 if(position == 0){
+                    isJumping = false;
                     clearInterval(downInterval);
                 }
-
             }, 20)
         } else{
-
             //irá subir
             position += 20;
             dino.style.bottom = position + "px";
